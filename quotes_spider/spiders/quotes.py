4a18fd7ae5bfc86a313ed9be23548032a65835c3
JeremyPie <91102181+JeremyPie@users.noreply.github.com>
2021-10-09 05:59:14 +0300

    Update quotes.py

--- quotes_spider/spiders/quotes.py
+++ quotes_spider/spiders/quotes.py
@@ -8,24 +8,26 @@ class QuotesSpider(Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        l = ItemLoader(item=QuotesSpiderItem(), response=response)
+        #l = ItemLoader(item=QuotesSpiderItem(), response=response)
         
-        h1_tag = response.xpath('//h1/a/text()').extract_first()
-        tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+        #h1_tag = response.xpath('//h1/a/text()').extract_first()
+        #tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
         
-        l.add_value('h1_tag', h1_tag)
-        l.add_value('tags', tags)
+        #l.add_value('h1_tag', h1_tag)
+        #l.add_value('tags', tags)
         
-        return l.load_item()
+        #return l.load_item()
         #yield {'H1 Tag': h1_tag, 'Tags': tags}
         
-        #quotes = response.xpath('//*[@class="quote"]')
-        #for quote in quotes:
-            #text = quote.xpath('.//*[@class="text"]/text()').extract_first()
-            #author = quote.xpath('.//*[@class="author"]/text()').extract_first()
-            #tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+            author = quote.xpath('.//*[@class="author"]/text()').extract_first()
+            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
             
-                  
+        yield {'Text': h1_tag, 
+               'Author': tags, 
+               'Tags': tags}          
             
         #next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
         #absolute_next_page_url = response.urljoin(next_page_url)
