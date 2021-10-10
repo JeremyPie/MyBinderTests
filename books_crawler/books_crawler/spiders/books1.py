16f47754860cf64b47687ab8158ece04e105a125
JeremyPie <91102181+JeremyPie@users.noreply.github.com>
2021-10-10 05:45:26 +0300

    Update books1.py

--- books_crawler/books_crawler/spiders/books1.py
+++ books_crawler/books_crawler/spiders/books1.py
@@ -1,12 +1,40 @@
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
+from scrapy import Spider
+from selenium import webdriver
+from scrapy.selector import Selector
+from scrapy.http import Request
+from time import sleep
+from selenium.common.exceptions import NosuchElementException
 
-class BooksSpider(CrawlSpider):
-    name = 'books'
+class BooksSpider(Spider):
+    name = 'books1'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/']
-
-    rules = (Rule(LinkExtractor(allow=('music')), callback='parse_page', follow=False),)
     
-    def parse_page(self, response):
+    def start_requests(self):
+        self.driver = webdriver.Chrome('path')
+        self.driver.get('site')
+        sel = Selector(text=self.driver.page_source)
+        books =  sel.xpath('//h3/a/@href').extract()
+        for book in books:
+            url = 'http://books.toscrape.com/' + book
+            yield Request(url, callback=self.parse_book)
+        
+        while True:
+            try:
+                next_page = self.driver.find_element_by_xpath('//a[text()="next"]').click()
+                sleep(3)
+                self.logger.info('Sleeping for 3 seconds')
+                next_page.click()
+                sel = Selector(text=self.driver.page_source)
+                books =  sel.xpath('//h3/a/@href').extract()
+                for book in books:
+                    url = 'http://books.toscrape.com/catalogue/' + book
+                    yield Request(url, callback=self.parse_book)
+                
+            except NoSuchElementException:
+                self.logger.info('No more pages to load.')
+                self.driver.quit()
+                break
+            
+                
+    def parse_book():
         pass
